45c538bfa4a9ffd5b8e3646d057c9bd101fdafd0
Gabriele Calvo <gcalvo87@gmail.com>
2021-12-27 21:24:21 +0000

    add bootstrap

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# random-picker
-An history-aware client-based random item picker
+# Random Team Member Picker
+
+A simple random item picker that is able to read and set historical pick information in the url.
+
+![Sample](sample.png)
+
+# Get started
+
+Simply go to [this url](https://htmlpreview.github.io/?https://github.com/gabrielecalvo/random-picker/blob/main/index.html) and setup your items to pick from.
+
+After that, you can simply save the url wherever you want and that will store the state:
+
+- last picked
+- number of times each item was picked
+
+# Example
+
+See [here](http://127.0.0.1:5500/index.html?last=null&items=George%3A2%2CRingo%3A2%2CPaul%3A2%2CJohn%3A2) for a pre-filled example with 4 team members.
--- script.js
+++ script.js
@@ -1,73 +1,117 @@
-function weightedRandom(items, weights) {
-    var i;
+let picked = null;
+let items = [];
 
-    for (i = 0; i < weights.length; i++)
-        weights[i] += weights[i - 1] || 0;
-    
-    var random = Math.random() * weights[weights.length - 1];
-    
-    for (i = 0; i < weights.length; i++)
-        if (weights[i] > random)
-            break;
-    
-    return items[i];
+function _weightedRandom(items, weights) {
+  var i;
+
+  for (i = 0; i < weights.length; i++) weights[i] += weights[i - 1] || 0;
+
+  var random = Math.random() * weights[weights.length - 1];
+
+  for (i = 0; i < weights.length; i++) if (weights[i] > random) break;
+
+  return items[i];
 }
 
-function parseQuery(queryString){
-    const urlParams = new URLSearchParams(queryString);
-    const itemParam = urlParams.get("items")
-    const lastPicked = urlParams.get("last") 
-    const itemStrArray = itemParam.split(",")
-    let items = []
+function _parseQuery(queryString) {
+  const urlParams = new URLSearchParams(queryString);
+  const itemParam = urlParams.get("items");
+  const lastPicked = urlParams.get("last");
+  if (itemParam != null) {
+    const itemStrArray = itemParam.split(",");
 
-    for(let idx in itemStrArray){
-        values = itemStrArray[idx].split(":")
-        let item = {
-            name: values[0],
-            count: parseInt(values[1]),
-            weight: 0
-        }
-        item.weight = (item.name == lastPicked) ? 0 : 1/Math.max(0.1, item.count)
-        items.push(item)
+    for (let idx in itemStrArray) {
+      values = itemStrArray[idx].split(":");
+      let item = {
+        name: values[0],
+        count: parseInt(values[1]),
+        weight: 0,
+      };
+      item.weight = item.name == lastPicked ? 0 : 1 / Math.max(0.1, item.count);
+      items.push(item);
     }
-    return items
+
+    document.getElementById("pick-btn").hidden = false;
+  }
 }
 
-function pick(){
-    picked = weightedRandom(items.map(i=>i.name), items.map(i=>i.weight))
-    document.getElementById("picked-text").innerHTML = `Fate chose <b>${picked}</b>. Do you accept or do you want to pick again?`
-    document.getElementById("accept-picked").hidden = false
-    document.getElementById("pick-btn").innerHTML = "Pick Again"
-    return picked
+function _createPiePlot(items) {
+  if (items.length > 0) {
+    document.getElementById("piechart-title").hidden = false;
+  }
+
+  var data = [
+    {
+      values: items.map((i) => i.weight),
+      labels: items.map((i) => i.name),
+      type: "pie",
+      textinfo: "label+percent",
+    },
+  ];
+  var layout = {
+    height: 400,
+    // width: 500,
+    margin: {
+      l: 20,
+      r: 20,
+      b: 20,
+      t: 20,
+      pad: 5,
+    },
+    title: false,
+    showlegend: false,
+  };
+  Plotly.newPlot("piechart", data, layout);
 }
 
-function acceptPicked(){
-    var url = new URL(window.location.href);
-    url.searchParams.set('last', picked)
-    count_params = items.map(i => {
-        newCount = i.name == picked ? i.count+1 : i.count
-        return `${i.name}:${newCount}`
-    }).join(',')
-    url.searchParams.set('items', count_params)
-    window.location.replace(url);
+function pick() {
+  picked = _weightedRandom(
+    items.map((i) => i.name),
+    items.map((i) => i.weight)
+  );
+  document.getElementById(
+    "picked-text"
+  ).innerHTML = `Fate chose <b>${picked}</b>. Do you accept or do you want to pick again?`;
+  document.getElementById("accept-picked").hidden = false;
+  document.getElementById("pick-btn").innerHTML = "Pick Again";
 }
 
-function createPiePlot(items){
-    var data = [{
-        values: items.map(i => i.weight),
-        labels: items.map(i => i.name),
-        type: 'pie',
-        textinfo: "label+percent",
-    }];
-    var layout = {
-        height: 400,
-        width: 500,
-        showlegend: false
-    };
-    Plotly.newPlot('piechart', data, layout);
+function acceptPicked() {
+  for (idx in items) {
+    let item = items[idx];
+    if (item.name == picked) {
+      item.count += 1;
+    }
+  }
+  setState(picked, items);
 }
 
-let items = []
-let picked = null
-items = parseQuery(window.location.search)
-createPiePlot(items)
+function setState(last, items) {
+  var url = new URL(window.location.href);
+  count_params = items
+    .map((i) => {
+      newCount = i.name == picked ? i.count + 1 : i.count;
+      return `${i.name}:${newCount}`;
+    })
+    .join(",");
+
+  url.searchParams.set("last", last);
+  url.searchParams.set("items", count_params);
+  window.location.replace(url);
+}
+
+function addItem() {
+  item_name = document.getElementById("new-item-input").value;
+  let item = {
+    name: item_name,
+    count: 0,
+    weight: 0,
+  };
+  console.log("adding item", item);
+  new_items = [...items, item];
+  console.log("new list of items", new_items);
+  setState(picked, new_items);
+}
+
+_parseQuery(window.location.search);
+_createPiePlot(items);
